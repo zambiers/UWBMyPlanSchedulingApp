4c7d54de61734eb70f7f7ca236611dc0842e7e05
Avah <114538590+zambiers@users.noreply.github.com>
2025-11-17 17:32:10 -0800

    Update courseScheduler.session.sql

--- courseScheduler.session.sql
+++ courseScheduler.session.sql
@@ -1,6 +1,3 @@
-Drop table if EXISTS SectionTime;
-Drop table if EXISTS Section;
-
 CREATE TABLE Students (
     StudentID INT PRIMARY KEY,
     FirstName VARCHAR(50) NOT NULL,
@@ -23,6 +20,8 @@ CREATE TABLE Professors (
     MajorOfInstruction VARCHAR(100) NOT NULL
 );
 CREATE TABLE Section (
+    SectionID INT AUTO_INCREMENT PRIMARY KEY,
+    CourseCode INT NOT NULL,
     SectionLetter CHAR(1) NOT NULL DEFAULT 'A',
     MaxEnrolled INT NOT NULL,
     EnrolledStudents INT DEFAULT 0,
@@ -31,14 +30,13 @@ CREATE TABLE Section (
     MeetingDay VARCHAR(10) NOT NULL,
     StartTime TIME NOT NULL,
     EndTime TIME NOT NULL,
-    PRIMARY KEY (SectionLetter),
     FOREIGN KEY (CourseCode) REFERENCES Courses(CourseCode),
     FOREIGN KEY (Instructor) REFERENCES Professors(EmployeeID)
 );
 CREATE TABLE DegreeProgram (
     DegreeName VARCHAR(70) PRIMARY KEY UNIQUE NOT NULL,
     DegreeType VARCHAR(5) NOT NULL CHECK (DegreeType IN ('Major', 'Minor')),
-    SchoolDivision VARCHAR(100) NOT NULL DUPLICATE,
-    EnrolledStudents INT DEFAULT '0',
-    Description TEXT NOT NULL DUPLICATE
+    SchoolDivision VARCHAR(100) NOT NULL,
+    EnrolledStudentsMax INT DEFAULT 0,
+    Description TEXT
 );
